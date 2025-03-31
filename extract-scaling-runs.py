944ae1f06ab0d217b179002d43a0927e36c52aaf
amartyads <amartyads@gmail.com>
2025-03-31 14:58:13 +0200

    options for custom folder

--- extract-scaling-runs.py
+++ extract-scaling-runs.py
@@ -4,6 +4,7 @@ import os
 import sys
 import glob
 import json
+import getopt
 import datetime as dt
 import pandas as pd
 
@@ -12,53 +13,81 @@ CUR_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(CUR_DIR))
 from helperscripts.utils import zeroPad
 
-with open('config.json') as jsonFile:
-    jsonData = json.load(jsonFile)
+def main(argv):
+    with open('config.json') as jsonFile:
+        jsonData = json.load(jsonFile)
+        
+    with open('config-scaling.json') as jsonFile:
+        scaleData = json.load(jsonFile)
     
-with open('config-scaling.json') as jsonFile:
-    scaleData = json.load(jsonFile)
+    #overwrite with args
+    helpText = f"""extract-scaling-runs.py
+              --boxSize <size of box in nm>
+              --dropDia <droplet diameter in nm>
+              --temp <temperature in reduced>
+              """
 
-folderName = "s" + str(jsonData["scenario"]["boxSize"]) + "d" + str(jsonData["scenario"]["dropDia"]) + "t" + str(jsonData["scenario"]["temperature"]).replace('.','_')
-startPath = os.getcwd()
-masterFolder = os.path.join(startPath,scaleData["masterPath"],folderName)
+    # input: scenario size, droplet diameter, autopas/ls1, 100save y/n, mamico y/n
+    try:
+        opts, args = getopt.getopt(argv, "hs:d:t:", ["help","boxSize=","dropDia=","temp="])
+    except:
+        print(helpText)
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            print(helpText)
+            sys.exit()
+        elif opt in ("-s", "--boxSize"):
+            jsonData['scenario']['boxSize'] = int(arg)
+        elif opt in ("-d", "--dropDia"):
+            jsonData['scenario']['dropDia'] = int(arg)
+        elif opt in ("-t","--temp"):
+            jsonData['scenario']['temperature'] = float(arg)
 
-os.chdir(masterFolder)
-numNodes = [2**x for x in range(scaleData["scaleStart"],scaleData["scaleSteps"])]
-numNodesSymbols = zeroPad(numNodes)
-runTypes = ["strong", "weak"]
+    folderName = "s" + str(jsonData["scenario"]["boxSize"]) + "d" + str(jsonData["scenario"]["dropDia"]) + "t" + str(jsonData["scenario"]["temperature"]).replace('.','_')
+    startPath = os.getcwd()
+    masterFolder = os.path.join(startPath,scaleData["masterPath"],folderName)
 
-rowsList = []
+    os.chdir(masterFolder)
+    numNodes = [2**x for x in range(scaleData["scaleStart"],scaleData["scaleSteps"])]
+    numNodesSymbols = zeroPad(numNodes)
+    runTypes = ["strong", "weak"]
 
-for config in scaleData["configs"]:
-    for runType in runTypes:
-        for numNode in numNodesSymbols:
-            chpath = os.path.join(config["name"],runType,numNode)
-            globobj = glob.glob(os.path.join(chpath,'output*'))
-            line = ''
-            lineWalltime = ''
-            walltimeCumulative = 0.0
-            if len(globobj) >= 1:
-                for i in range(len(globobj)):
-                    with open(globobj[i], encoding="utf-8") as file:
-                        for line in file:
-                            if 'Finished all coupling cycles' in line:
-                                lineWalltime = line
-                                break
-                    if lineWalltime == '':
-                        print(f"Error in {globobj[i]}")
-                        sys.exit(2)
-                    else:
-                        stringWalltime = lineWalltime[lineWalltime.find('after ')+5:-2].strip()
-                        walltimeCumulative += float(stringWalltime)
-            else:
-                print(f"Error in {config["name"]},{runType},{numNode}")
-                continue
-                #print(rowsList)
-                #sys.exit(2)
-            dict = {'Folder':chpath,'Config':config["name"],'RunType':runType,'NumNodes':numNode,'CumulativeWalltime':walltimeCumulative,'NoOutputs':len(globobj),'AvgWalltime':walltimeCumulative/len(globobj)}
-            rowsList.append(dict)
+    rowsList = []
 
-df = pd.DataFrame(rowsList, columns=['Folder','Config','RunType','NumNodes','CumulativeWalltime','NoOutputs','AvgWalltime'])
-df.set_index('Folder', inplace=True)
-print(df)
-df.to_csv('./outputs'+ folderName +'.csv')
+    for config in scaleData["configs"]:
+        for runType in runTypes:
+            for numNode in numNodesSymbols:
+                chpath = os.path.join(config["name"],runType,numNode)
+                globobj = glob.glob(os.path.join(chpath,'output*'))
+                line = ''
+                lineWalltime = ''
+                walltimeCumulative = 0.0
+                if len(globobj) >= 1:
+                    for i in range(len(globobj)):
+                        with open(globobj[i], encoding="utf-8") as file:
+                            for line in file:
+                                if 'Finished all coupling cycles' in line:
+                                    lineWalltime = line
+                                    break
+                        if lineWalltime == '':
+                            print(f"Error in {globobj[i]}")
+                            sys.exit(2)
+                        else:
+                            stringWalltime = lineWalltime[lineWalltime.find('after ')+5:-2].strip()
+                            walltimeCumulative += float(stringWalltime)
+                else:
+                    print(f"Error in {config['name']},{runType},{numNode}")
+                    continue
+                    #print(rowsList)
+                    #sys.exit(2)
+                dict = {'Folder':chpath,'Config':config["name"],'RunType':runType,'NumNodes':numNode,'CumulativeWalltime':walltimeCumulative,'NoOutputs':len(globobj),'AvgWalltime':walltimeCumulative/len(globobj)}
+                rowsList.append(dict)
+
+    df = pd.DataFrame(rowsList, columns=['Folder','Config','RunType','NumNodes','CumulativeWalltime','NoOutputs','AvgWalltime'])
+    df.set_index('Folder', inplace=True)
+    print(df)
+    df.to_csv('./outputs'+ folderName +'.csv')
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
